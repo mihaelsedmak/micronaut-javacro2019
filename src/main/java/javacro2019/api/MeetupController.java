2307b596fa8da372a15b5fb553ebff4464763593
Mihael <mihael.sedmak@gmail.com>
2019-05-07 23:42:52 +0200

    cleanup

--- src/main/java/javacro2019/api/MeetupController.java
+++ src/main/java/javacro2019/api/MeetupController.java
@@ -2,17 +2,21 @@ package javacro2019.api;
 
 import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpResponseFactory;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import javacro2019.client.RoboHashOperations;
+import io.micronaut.validation.Validated;
+import io.micronaut.views.View;
 import javacro2019.client.MeetupOperations;
+import javacro2019.client.RoboHashOperations;
 import javacro2019.model.Meetup;
+
 import javax.inject.Inject;
+import javax.validation.constraints.NotBlank;
 
 @Controller("/meetup")
+@Validated
 public class MeetupController {
 
     @Inject
@@ -24,23 +28,26 @@ public class MeetupController {
     @Value("${micronaut.meetup.urlname}")
     private String meetupUrlname;
 
-    @Value("${micronaut.meetup.id}")
-    private String meetupId;
-
-
-    @Get("/robot")
+    @Get("/{meetupId}/host")
     @Produces(MediaType.IMAGE_PNG)
-    public byte[] roboMeetup() {
+    public byte[] roboMeetup(@NotBlank final String meetupId) {
         Meetup meetup = meetupOperations.getMeetup(meetupUrlname, meetupId);
         byte[] robot = roboHashOperations.getRobot(meetup.toString().replaceAll(" ", "+"));
         return robot;
     }
 
-    @Get("/plain")
-    @Produces(MediaType.TEXT_HTML)
-    public String plainMeetup() {
+    @Get("/{meetupId}/basic")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String plainMeetup(@NotBlank final String meetupId) {
         Meetup meetup = meetupOperations.getMeetup(meetupUrlname, meetupId);
         return meetup.toString();
     }
+
+    @Get("/{meetupId}/visit")
+    @View("meetup")
+    public HttpResponse visitMeetup(@NotBlank final String meetupId) {
+        Meetup meetup = meetupOperations.getMeetup(meetupUrlname, meetupId);
+        return HttpResponse.ok(meetup);
+     }
 
 }
